51ede57891d590bb1dfb32c5c5a161e6f815141c
Tapan Ghataliya <tapanghataliya@Tapans-MacBook-Air.local>
2024-02-01 20:00:11 +0530

    display fullname project

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function App() {
+  const [first, setFirst] = useState("");
+  const [last, setLast] = useState("");
+  const [fullName, setFullName] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    setFullName(`Full Name: ${first} ${last}`);
+  }
+  return (
+    <div>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+            First Name:
+            <input
+              value={first}
+              type="text"
+              onChange={(e) => setFirst(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            Last Name:
+            <input
+              value={last}
+              type="text"
+              onChange={(e) => setLast(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </div>
+        <div>
+          {fullName}
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
